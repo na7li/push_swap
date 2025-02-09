cba9b51bd8bf26dc5f6a1d9b912287272813af14
Mohamed Nahli <mnahli@c2r1p10.1337.ma>
2025-02-09 14:17:55 +0100

    done

--- Makefile
+++ Makefile
@@ -2,20 +2,16 @@ CC                  = cc
 CFLAGS              = -Wall -Werror -Wextra
 RM                  = rm -rf
 
-SRC_DIR             = srcs/
+SRC_DIR             = mandatory/
 NAME                = push_swap
-BONUS				= checker
 
 MAIN		= $(SRC_DIR)MainLogic/push_swap.c
 MAIN_OBJ	= $(MAIN:%.c=%.o)
 
 SRCS		= $(SRC_DIR)MainLogic/pushswap_utils.c \
 			  $(SRC_DIR)Parsing/input_validation.c $(SRC_DIR)Parsing/parsing.c \
-			  $(SRC_DIR)SortStacks/sort_large_stack_1.c $(SRC_DIR)SortStacks/sort_large_stack_2.c \
-			  $(SRC_DIR)SortStacks/sort_small_stack.c \
-			  $(SRC_DIR)StackUtils/push.c $(SRC_DIR)StackUtils/rotate.c \
-			  $(SRC_DIR)StackUtils/rrotate.c $(SRC_DIR)StackUtils/stack_utils.c \
-			  $(SRC_DIR)StackUtils/swap.c \
+			  $(SRC_DIR)SortStacks/sort_large_stack_1.c $(SRC_DIR)SortStacks/sort_large_stack_2.c $(SRC_DIR)SortStacks/sort_small_stack.c \
+			  $(SRC_DIR)StackUtils/push.c $(SRC_DIR)StackUtils/rotate.c $(SRC_DIR)StackUtils/rrotate.c $(SRC_DIR)StackUtils/stack_utils.c $(SRC_DIR)StackUtils/swap.c \
 			  $(SRC_DIR)Utilities/ft_atol.c $(SRC_DIR)Utilities/ft_split.c $(SRC_DIR)Utilities/number_of_digits.c
 SRCS_OBJ	= $(SRCS:%.c=%.o)
 
@@ -36,4 +32,4 @@ fclean: clean
 
 re: fclean all
 
-.PHONY: all bonus clean fclean re de
+.PHONY: all bonus clean fclean re
---
+++ mandatory/StackUtils/push.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnahli <mnahli@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/01 10:15:34 by mnahli            #+#    #+#             */
+/*   Updated: 2025/02/09 09:20:48 by mnahli           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../push_swap.h"
+
+int	push(t_stack **from, t_stack **to)
+{
+	t_stack	*from_1;
+
+	if (!from || !*from || !to)
+		return (0);
+	from_1 = *from;
+	(*from) = (*from)->next;
+	from_1->next = (*to);
+	(*to) = from_1;
+	return (1);
+}
+
+void	pa(t_stack **a, t_stack **b)
+{
+	if (push(b, a))
+		write(1, "pa\n", 3);
+}
+
+void	pb(t_stack **a, t_stack **b)
+{
+	if (push(a, b))
+		write(1, "pb\n", 3);
+}
